4dda418cfa4d07e7c2191f41b5ab3b501afbdbc4
dnn <dnnbaq@gmail>
2024-01-22 21:42:49 -0500

    hyperlinks

--- email.py
+++ email.py
@@ -1,15 +1,15 @@
 from email.message import EmailMessage
 import smtplib
 
-PRG_EMAIL = "admin@admin.com"
-PRG_PASS = "admin"
+PRG_EMAIL = "delete.bins@gmail.com"
+PRG_PASS = "gcpi mepa cxqm lqsa"
 TAG_PROGRAM_NAME = "SCHMOOZE"
 TAG_SUBJECT = " sent you an invitation on "
 TAG_BODY_HEADER = " invited you to an event on "
 TAG_SELECT_TIME = "Please select a time:"
 TAG_SELECT_RESERVATION = "Please select a reservation:"
 
-def get_HTML(locations, times) -> str:
+def get_HTML(locations: dict, times: dict) -> str:
     html = f"<p>{TAG_SELECT_TIME}</p>\n"
     for t in times.keys():
         html += f"""<p style="text-align: center;"><a href="{times[t]}"><strong>{t}</strong></a></p>\n"""
@@ -17,9 +17,9 @@ def get_HTML(locations, times) -> str:
     for l in locations.keys():
         html += f"""<p style="text-align: center;"><img src="{locations[l][1]}" alt=""/></p>
 <p style="text-align: center;"><a href="{locations[l][0]}">{l}</a></p>\n\n"""
-    return html
+    return f"<html>\n<body>\n{html}</body>\n</html>"
 
-def get_RAW(locations, times) -> str:
+def get_RAW(locations: dict, times: dict) -> str:
     raw = f"{TAG_SELECT_TIME}\n\n"
     for t in times.keys():
         raw += f"{t}: {times[t]}\n"
@@ -28,9 +28,9 @@ def get_RAW(locations, times) -> str:
         raw += f"{l} ({locations[l][1]}):\n{locations[l][0]}\n\n"
     return raw
     
-def email(ADDRESS="", PASSWORD="", subject="", email_raw="", email_html="", Bcc=True, *recipients) -> bool:
+def email(ADDRESS="", PASSWORD="", subject="", email_raw="", email_html="", Bcc=True, recipients=[]) -> bool:
     
-    if ADDRESS == "" or PASSWORD == "" or email_raw == "" or recipients == []: return False
+    if ADDRESS == "" or PASSWORD == "" or email_raw == "" or recipients == None: return False
 
     x = 'Bcc'
     y = 'To'
@@ -63,8 +63,14 @@ def send_email(SENDER:str, DATE: str, RECIPIENTS: list, locations: dict, times: 
     # times dict format: {time: voting_link, ...}
     
     SUBJECT = f"[{TAG_PROGRAM_NAME}] {SENDER}{TAG_SUBJECT}{DATE}!"
-    RAW = "{SENDER}{TAG_SUBJECT}{DATE}.\n\n"
-    RAW = RAW + get_RAW(locations, times)
-    HTML = get_HTML(locations, times)
+    RAW = f"{SENDER}{TAG_SUBJECT}{DATE}.\n\n"
+    RAW += get_RAW(locations, times)
+    HTML = f"<p>{SENDER}{TAG_SUBJECT}{DATE}.</p>\n"
+    HTML += get_HTML(locations, times)
     
     return email(ADDRESS=PRG_EMAIL, PASSWORD=PRG_PASS, subject=SUBJECT, email_raw=RAW, email_html=HTML, recipients=RECIPIENTS)
+
+
+print("starting")
+send_email("DEAN B.", "12/29/04", ['sid77449@gmail.com', 'npcasco22@gmail.com', 'dnnbaq@gmail.com', 'dlb330@scarletmail.rutgers.edu'], {"Five Guys": ['youtube.com', "https://s3-media0.fl.yelpcdn.com/bphoto/TYd2IE1UKNlAWwCyLmILmA/348s.jpg"], "Subway": ['youtube.com', "https://a.mktgcdn.com/p/kcIdWEAbf_C1nNLIlVecIVGtR48A6MLeiSCbTY_QoJE/1050x906.jpg"]}, {"11:00 AM": "youtube.com", "01:30 PM": "youtube.com"})
+print("finished")
