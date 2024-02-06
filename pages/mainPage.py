cfd5351d57c7f0d1c3410a36d2feee59de2e7195
dnn <dnnbaq@gmail>
2024-02-06 17:01:07 -0500

    prints_events_dict

--- pages/mainPage.py
+++ pages/mainPage.py
@@ -176,7 +176,7 @@ def mainPage():
 
             # Proceed with serialization and emailing
 
-            st.session_state["events_dict"] = {
+            events = {
                 'uuid': str(uuid),
                 'date': selected_date.strftime('%Y-%m-%d'),  # Convert to string in YYYY-MM-DD format
                 'times': [str(e) for e in selected_time_slots],
@@ -187,13 +187,15 @@ def mainPage():
                 'votes': {}
             }
 
-            edb.event.details.serialize(st.session_state["events_dict"])
+            print(events)
+
+            edb.event.details.serialize(events)
             
-            st.write(st.session_state["events_dict"])
+            st.write(events)
             
             st.toast("Invite sent successfully!")
 
-            notify.send.invite(st.session_state["events_dict"]["uuid"], f"http://schmooze.us.to/?uuid={uuid}", True) # temporary placeholder link for the voting page
+            notify.send.invite(uuid, f"http://schmooze.us.to/?uuid={uuid}", True) # temporary placeholder link for the voting page
 
 
 if 'authentication_status' in st.session_state and st.session_state['authentication_status']:
